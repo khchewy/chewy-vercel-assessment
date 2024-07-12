13c2c2c9196d24ee5b50a4ec63449e85a5339b47
khchewy <141249764+khchewy@users.noreply.github.com>
2024-07-13 00:29:45 +1000

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -37,7 +37,7 @@ export function generateMetadata({ params }) {
       description,
       type: 'article',
       publishedTime,
-      url: `${baseUrl}/questions/${post.slug}`,
+      url: `${baseUrl}/blog/${post.slug}`,
       images: [
         {
           url: ogImage,
@@ -77,7 +77,7 @@ export default function Blog({ params }) {
             image: post.metadata.image
               ? `${baseUrl}${post.metadata.image}`
               : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-            url: `${baseUrl}/questions/${post.slug}`,
+            url: `${baseUrl}/blog/${post.slug}`,
             author: {
               '@type': 'Chewy',
               name: 'Vercel Technical Assessment',
