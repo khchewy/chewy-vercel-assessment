f0b428732642a3df17964158a65e90075f171152
khchewy <141249764+khchewy@users.noreply.github.com>
2024-07-13 00:38:04 +1000

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://github.com/khchewy/chewy-vercel-assessment"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -35,10 +35,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://vercel.com/chewys-projects-834cd7f5/chewy-vercel-assessment"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">vercel project</p>
           </a>
         </li>
       </ul>
