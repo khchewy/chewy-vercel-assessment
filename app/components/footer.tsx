926b5a013c07b7a54fe779b613199ce62edd6c0c
khchewy <141249764+khchewy@users.noreply.github.com>
2024-07-12 18:29:54 +1000

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,6 +19,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <!--
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -30,6 +31,7 @@ export default function Footer() {
             <p className="ml-2 h-7">rss</p>
           </a>
         </li>
+        -->
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
